81f635f72a4036afe6f835c6007bd4106d268754
Tarxos <ftarxos@gmail.com>
2022-08-07 21:31:30 +0300

    correct samples

---
+++ Messaging.Receiver/Samples/HelloWorld.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Messaging.Receiver.Samples;
+
+/// <summary>
+/// Receive one message and shutdown
+/// </summary>
+internal class HelloWorld
+{
+    private readonly NotifyProvider _provider;
+    private readonly IModel _channel;
+    private const string Queue = "hello";
+    public HelloWorld(NotifyProvider provider)
+    {
+        _provider = provider;
+        
+        var channel = _channel = provider.ConfigureChannel(Queue);
+        channel.QueueDeclare(
+            queue: Queue,
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+    }
+
+    public void Subscribe()
+    {
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += HandleMessage;
+        _provider.StartReceiving(Queue, true, consumer);
+    }
+
+    private void HandleMessage(object? _, BasicDeliverEventArgs e)
+    {
+        var message = Encoding.UTF8.GetString(e.Body.Span);
+        Console.WriteLine(" [x] Received {0}", message);
+    }
+}
---
+++ Messaging.Receiver/Samples/Worker.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Messaging.Receiver.Samples;
+
+/// <summary>
+/// Will receive messages that will be handled by only one receiver that take message first
+/// </summary>
+internal class Worker
+{
+    private readonly NotifyProvider _provider;
+    private readonly IModel _channel;
+    private const string Queue = "hello";
+
+    public Worker(NotifyProvider provider)
+    {
+        _provider = provider;
+        
+        var taskChannel = _channel = provider.ConfigureChannel("task_queue");
+        taskChannel.QueueDeclare(queue: "task_queue",
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+        taskChannel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+    }
+
+    public void Subscribe()
+    {
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += HandleMessage;
+        _provider.StartReceiving(Queue, false, consumer);
+    }
+
+    private void HandleMessage(object? sender, BasicDeliverEventArgs e)
+    {
+        var message = Encoding.UTF8.GetString(e.Body.Span);
+        Console.WriteLine(" [x] Received {0}", message);
+
+        int dots = message.Split('.').Length - 1;
+        Thread.Sleep(dots * 1000);
+
+        Console.WriteLine(" [x] Done");
+
+        // Note: it is possible to access the channel via
+        //       ((EventingBasicConsumer)sender).Model here
+        _channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+    }
+}
