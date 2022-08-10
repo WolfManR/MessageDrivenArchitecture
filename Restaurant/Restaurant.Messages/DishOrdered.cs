635fb5d08153553f24b80a74a7fe566d129e8a06
Tarxos <ftarxos@gmail.com>
2022-08-10 22:13:12 +0300

    add events models

---
+++ Restaurant/Restaurant.Messages/DishOrdered.cs
@@ -0,0 +1,20 @@
+﻿namespace Restaurant.Messages;
+
+public interface IDishOrdered
+{
+    public Guid OrderId { get; }
+        
+    public Dish Dish { get; }
+}
+
+public class DishOrdered : IDishOrdered
+{
+    public DishOrdered(Guid orderId, Dish dish)
+    {
+        OrderId = orderId;
+        Dish = dish;
+    }
+
+    public Guid OrderId { get; }
+    public Dish Dish { get; }
+}
