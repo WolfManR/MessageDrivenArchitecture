9974e13de1cb4ee2bcf7f54c6706fd719dcf0a03
Tarxos <ftarxos@gmail.com>
2022-08-28 16:25:17 +0300

    clear console app

--- Restourant/Program.cs
+++ Restourant/Program.cs
@@ -1,64 +1,62 @@
 ﻿using System.Diagnostics;
 using System.Text;
 using ConsoleMenu;
+using Refit;
 
 Console.OutputEncoding = Encoding.UTF8;
-using var rest = new Hall();
-
-Menu bookTableMenu = new();
-bookTableMenu.SubTitle = "выберите способ уведомления";
-bookTableMenu.AddMenuItem("мы уведомим Вас по смс (асинхронно)", BookTable(rest.BookFreeTableAsync).LogExecuteTime);
-bookTableMenu.AddMenuItem("подождите на линии, мы Вас оповестим (синхронно)", BookTable(rest.BookFreeTable).LogExecuteTime);
-bookTableMenu.AddMenuItem("обратно", bookTableMenu.Back);
-
-Menu freeTableMenu = new();
-freeTableMenu.SubTitle = "выберите способ уведомления";
-freeTableMenu.AddMenuItem("мы уведомим Вас по смс (асинхронно)", FreeTable(rest.FreeTableAsync).LogExecuteTime);
-freeTableMenu.AddMenuItem("подождите на линии, мы Вас оповестим (синхронно)", FreeTable(rest.FreeTable).LogExecuteTime);
-freeTableMenu.AddMenuItem("обратно", freeTableMenu.Back);
-freeTableMenu.AddMenuItem("Выход", freeTableMenu.Close);
+var bookingClient = RestService.For<IBookingApi>("");
 
 Menu mainMenu = new();
 mainMenu.SubTitle = "Привет! Желаете забронировать или освободить столик?";
-bookTableMenu.ParentMenu = mainMenu;
-freeTableMenu.ParentMenu = mainMenu;
-
-mainMenu.AddMenuItem("Забронировать", bookTableMenu.ShowMenu);
-mainMenu.AddMenuItem("Освободить", freeTableMenu.ShowMenu);
+mainMenu.AddMenuItem("Забронировать", BookTable);
+mainMenu.AddMenuItem("Освободить", FreeTable);
 mainMenu.AddMenuItem("Выход", mainMenu.Close);
 mainMenu.ShowMenu();
 
-Action BookTable(Action<int> bookBehavior)
+async void BookTable()
 {
-    return () =>
-    {
-        Console.WriteLine("На сколько мест столик желаете:");
-        int.TryParse(Console.ReadLine(), out var places);
-        bookBehavior.Invoke(places);
-    };
+    Console.WriteLine("На сколько мест столик желаете:");
+    int.TryParse(Console.ReadLine(), out var places);
+    var orderId = await bookingClient.Book(places);
+    State.OrderId = orderId;
 }
 
-Action FreeTable(Action<int> freeBehavior)
+async void FreeTable()
 {
-    return () =>
+    if (State.OrderId == Guid.Empty)
     {
-        Console.WriteLine("Укажите номер столика");
-        int.TryParse(Console.ReadLine(), out var tableId);
-        freeBehavior.Invoke(tableId);
-    };
-}
-
-static class LogExtension
-{
-    public static void LogExecuteTime(this Action execution)
-    {
-        var stopWatch = new Stopwatch();
-        stopWatch.Start();
-    
-        execution.Invoke();
-    
-        stopWatch.Stop();
-        var ts = stopWatch.Elapsed;
-        Console.WriteLine($"{ts.Seconds:80}:{ts.Milliseconds:00}");
+        Console.WriteLine("Вы ещё не забронировали столик");
+        return;
     }
+
+    await bookingClient.Free(State.OrderId);
+    State.OrderId = Guid.Empty;
+}
+
+static void LogExecuteTime(Action execution)
+{
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+
+    execution.Invoke();
+
+    stopWatch.Stop();
+    var ts = stopWatch.Elapsed;
+    Console.WriteLine($"{ts.Seconds:80}:{ts.Milliseconds:00}");
+}
+
+static class State
+{
+    public static Guid OrderId { get; set; }
+}
+
+    
+
+interface IBookingApi
+{
+    [Post("")]
+    Task<Guid> Book(int countOfPersons);
+
+    [Post("")]
+    Task Free(Guid orderId);
 }
