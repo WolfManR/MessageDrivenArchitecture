dc25b1f6d920f53407a900539c0b8ba60af19787
Tarxos <ftarxos@gmail.com>
2022-09-04 21:30:30 +0300

    add prototype of restaurant tables dashboard to blazor app

---
+++ src/Restaurant.Contracts/IBookingClient.cs
@@ -0,0 +1,15 @@
+﻿using Refit;
+
+namespace Restaurant.Contracts;
+
+public interface IBookingClient
+{
+    [Get("/tables")]
+    Task<IReadOnlyList<TableResponse>> Tables();
+
+    [Post("/book")]
+    Task<BookingResponse> Book([Body] BookingRequest request);
+
+    [Post("/free")]
+    Task Free([Body] CancelBookingRequest request);
+}
